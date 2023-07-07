cef3a3755a3fad9c8bfdedeb43e7e6940149bb15
vrk <victoriakirst@gmail.com>
2023-07-07 14:00:28 -0400

    update everything

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
 
 import packageJson from "./package.json" assert { type: "json" };
 
@@ -23,12 +24,14 @@ export default [
     plugins: [
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json"})
+      typescript({ tsconfig: "./tsconfig.json"}),
+      postcss()
      ]
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{file: "dist/index.d.ts", format: "esm"}],
-    plugins: [dts()]
+    plugins: [dts()],
+    external: [/\.css$/],
   }
 ];
