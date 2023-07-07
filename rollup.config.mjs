d748c09b7cdf20d8d917713f8080abe1b40bb486
vrk <victoriakirst@gmail.com>
2023-07-07 19:54:23 -0400

    fonts

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
+import url from 'rollup-plugin-url'
 
 import packageJson from "./package.json" assert { type: "json" };
 
@@ -25,7 +26,14 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json"}),
-      postcss()
+      postcss(),
+      url({
+        // by default, rollup-plugin-url will not handle font files
+        include: ['**/*.woff', '**/*.woff2', "**/*.ttf"],
+        // setting infinite limit will ensure that the files 
+        // are always bundled with the code, not copied to /dist
+        limit: Infinity,
+      }),
      ]
   },
   {
