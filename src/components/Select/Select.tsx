51f4293e59296a11eea018a34803c1d41f6c01c0
vrk <victoriakirst@gmail.com>
2023-07-10 18:13:44 -0400

    add ref

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import styles from './Select.module.css'; // Import css modules stylesheet as styles
 
 export interface SelectProps {
-  ref?: React.RefObject<HTMLInputElement>;
+  ref?: React.RefObject<HTMLSelectElement>;
 }
 
-const Select = ({ children }: React.PropsWithChildren<SelectProps>) => {
+const Select = ({ children, ref }: React.PropsWithChildren<SelectProps>) => {
   const style = {
   }
-  return <select className={styles.component} style={style}>
+  return <select className={styles.component} style={style} ref={ref}>
     {children}
   </select>
 }
