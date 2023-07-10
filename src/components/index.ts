eda4926fd747f1bcf810bc28123808039418afe3
vrk <victoriakirst@gmail.com>
2023-07-10 17:20:22 -0400

    add printer

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export { default as Button } from './Button';
 export { default as CloudHeader } from './CloudHeader';
 export { default as DitherButton } from './DitherButton';
 export { default as PaperIcon } from './PaperIcon';
+export { default as Printer } from './Printer';
 export { default as RadioButton } from './RadioButton';
 export { default as Slider } from './Slider';
 export { default as Toggle } from './Toggle';
