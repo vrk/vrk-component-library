661a8163be8afd80414ed9266602cbcab6e46364
vrk <victoriakirst@gmail.com>
2023-07-10 17:41:15 -0400

    shared button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import styles from './Button.module.css'; // Import css modules stylesheet as styles
+// import styles from './Button.module.css'; // Import css modules stylesheet as styles
+import styles from '../../shared/BasicButton.module.css'; // Import css modules stylesheet as styles
 
 export interface ButtonProps {
   label: string;
@@ -9,23 +10,6 @@ export interface ButtonProps {
   color?: "yellow"|"pink"
 }
 
-const colorSets = {
-  yellow: {
-    face: "#84433B",
-    border: "#84433B",
-    background: "#FDE5B4",
-    insetBottom: "#F9A806",
-    insetTop: "#FFF" 
-  },
-  pink: {
-    face: "#F15AA3",
-    border: "#D4126F",
-    background: "#FEE6F2",
-    insetBottom: "#F2A6CC",
-    insetTop: "#FFF" 
-  }
-}
-
 const Button = ({
   label,
   topBottomPadding = 6,
@@ -33,17 +17,12 @@ const Button = ({
   fontSize = 18,
   color = "yellow"
 }: ButtonProps) => {
-  const colorSet = colorSets[color];
-
   const style = {
     fontSize: `${fontSize}px`,
     padding: `${topBottomPadding}px ${leftRightPadding}px`,
-    color: colorSet.face,
-    border: `1px solid ${colorSet.border}`,
-    background: colorSet.background,
-    boxShadow: `0px -3px 0px 0px ${colorSet.insetBottom} inset, -2px 2px 0px 0px ${colorSet.insetTop} inset`
   }
-  return <button className={styles.button} style={style}>{label}</button>
+  const className = `${styles.button} ${styles[color]}`;
+  return <button className={className} style={style}>{label}</button>
 }
 
 export default Button;
