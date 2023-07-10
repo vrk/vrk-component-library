9370ce075128b77c0ccda4a7fc73cd4c28a46cc1
vrk <victoriakirst@gmail.com>
2023-07-10 08:24:28 -0400

    yellow butotn

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from './Button.module.css'; // Import css modules stylesheet as styles
-import logo from '../../assets/images/bento.png';
 
 export interface ButtonProps {
   label: string;
@@ -9,7 +8,6 @@ export interface ButtonProps {
 const Button = (props: ButtonProps) => {
   return <div>
     <button className={styles.button}>{props.label}</button>
-    <img src={logo}/>
   </div>
 }
 
